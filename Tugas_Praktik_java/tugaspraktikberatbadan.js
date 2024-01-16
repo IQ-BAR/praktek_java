967a050c4aef2db2aa429eff3b405ad948e51824
Fairus <142189063+IQ-BAR@users.noreply.github.com>
2024-01-16 15:22:50 +0800

    Add files via upload

---
+++ Tugas_Praktik_java/tugaspraktikberatbadan.js
@@ -0,0 +1,11 @@
+let beratM = 43;
+let tinggiM = 1.53;
+let beratJ = 54;
+let tinggiJ = 1.56;
+let mINT = beratM/tinggiM **2;
+let jINT = beratJ/tinggiJ **2;
+let hasilPerhitunganIMT = mINT > jINT;
+
+console.log(`M INT : ${mINT}
+J INT : ${jINT}
+HasilPerbandingan IMT M dan J : ${hasilPerhitunganIMT}`);
