c028baa9265c5ac6e237964233b5008a8c9ea42c
Fairus <142189063+IQ-BAR@users.noreply.github.com>
2024-01-22 10:44:53 +0800

    Add files via upload

---
+++ Tugas_Praktik_java/tugas_4.js
@@ -0,0 +1,26 @@
+let beratM = 43;
+let tinggiM = 1.53;
+let beratJ = 54;
+let tinggiJ = 1.56;
+let mINT = beratM/tinggiM **2;
+let jINT = beratJ/tinggiJ **2;
+let hasilPerhitunganIMT = mINT > jINT;
+
+console.log(`M INT : ${mINT}
+J INT : ${jINT}
+HasilPerbandingan IMT M dan J : ${hasilPerhitunganIMT}`);
+console.log()
+
+console.log(`_________________________________Tugas 4_________________________________`)
+
+console.log()
+
+if(mINT > jINT){
+    console.log(`Nilai IMT Madan ${mINT} lebih tinggi dari joan ${jINT}`);
+    
+
+}else{
+    console.log(`Nilai IMT Madan ${mINT} Lebih Tinggi Dari Joan ${jINT}`)
+};
+    
+    
